b356f36a9f813597a2cf2cc08cda3cd001458de0
epidersis <32263760+epidersis@users.noreply.github.com>
2018-04-25 19:09:39 +0900

    25.04.2018 : Sorry, this one is the last

---
+++ 337A.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+
+void lin_sort(short *array, short size)
+{
+	short max;
+	
+	for (short i = 0; i < size; i++)
+	{
+		max = i;
+		
+		for (short j = i; j < size; j++)
+		{
+			if (array[j] > array[max])
+			{
+				max = j;
+			}
+		}
+		
+		short temp = array[i];
+		array[i] = array[max];
+		array[max] = temp;
+	}
+}
+
+
+int main()
+{
+	short n, m, difference = 1000;
+	short array[1000];
+	
+	cin >> n >> m;
+	
+	for (int i = 0; i < m; i++)
+	{
+		cin >> array[i];
+	}
+	
+	lin_sort(array, m);
+	
+	for (int i = 0; i < m - n + 1; i++)
+	{
+		if (array[i] - array[i+n-1] < difference)
+		{
+			difference = array[i] - array[i+n-1];
+		}
+	}
+	
+	cout << difference;
+	
+	return 0;
+}
