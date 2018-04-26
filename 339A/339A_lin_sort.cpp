fc5085341a6681705e6b9d90f779234734eabdab
epidersis <epidersis@gmail.com@>
2018-04-26 23:13:28 +0900

    Хорошая идея от Матича

---
+++ 339A/339A_lin_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+{
+	char s[100];
+	cin >> s;	
+	
+	for (short j = 0; j < strlen(s); j += 2)
+	{
+		short min = j;
+		
+		for (short i = j; i < strlen(s); i += 2)
+		{	
+			if (s[min] > s[i])
+			{
+				min = i;
+			}		
+		}
+		
+		swap(s[min], s[j]);
+	}
+	cout << s;
+}
