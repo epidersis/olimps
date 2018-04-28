573969176aeab5a5fdf4b2f39028580dbdb9970e
epidersis <epidersis@gmail.com@>
2018-04-28 14:57:24 +0900

    Chess

--- 519A.cpp
+++ 519A.cpp
@@ -8,29 +8,26 @@ int main()
 	char f;
 	short weight = 0, side;
 	
-	for (short i = 0; i < 8; i++)
+	for (short i = 0; i < 64; i++)
 	{
-		for (short j = 0; j < 8; j++)
+		cin >> f;
+		
+		if (f == '.') continue;
+		
+		if (f <= 90)
 		{
-			cin >> f;
-			
-			if (f == '.') continue;
-			
-			if (f <= 90)
-			{
-				side = 1;
-				f += 32;
-			}
-			else
-			{
-				side = -1;
-			}
-			
-			if (f == 'q') weight += 9*side;
-			else if (f == 'r') weight += 5*side;
-			else if (f == 'b' || f == 'n') weight += 3*side;
-			else if (f == 'p') weight += side;
+			side = 1;
+			f += 32;
 		}
+		else
+		{
+			side = -1;
+		}
+		
+		if (f == 'q') weight += 9*side;
+		else if (f == 'r') weight += 5*side;
+		else if (f == 'b' || f == 'n') weight += 3*side;
+		else if (f == 'p') weight += side;
 	}
 	
 	if (weight > 0) cout << "White";
