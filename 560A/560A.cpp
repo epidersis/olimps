2da8538d919043bdc51486ad23c1196b8a659cd3
epidersis <epidersis@gmail.com@>
2018-04-30 12:10:48 +0900

    Матич предлагает

---
+++ 560A/560A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main ()
+{
+	short n;
+	
+	cin >> n;
+	
+	int a[n];
+	
+	for ( int i = 0; i < n; i++ )
+	{
+		cin >> a[i];
+	}	
+	
+	for ( int i = 0; i < n; i++ )
+	{
+		if ( a[i] == 1 ) 
+		{
+			cout << -1;
+			return 0;
+		}
+	}
+	cout << 1;
+}
---
+++ 560A/560A.py
@@ -0,0 +1,4 @@
+n = int(input())
+notes = list(map(int, input().split(' ')))
+
+print(-1) if 1 in notes else print(1)
