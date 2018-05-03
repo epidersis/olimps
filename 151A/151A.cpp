6f17624d9a3a570739335a9e5e72efc2da0cf0f9
epidersis <epidersis@gmail.com@>
2018-05-03 16:26:30 +0900

    Another one

--- 151A/151A.cpp
+++ 151A/151A.cpp
@@ -7,12 +7,12 @@ int main()
 {
     int n, k, l, c, d, p, nl, np;
     cin >> n >> k >> l >> c >> d >> p >> nl >> np;
-    
+
     int toast = k*l / (n * nl);
     toast = c*d/n < toast ?  c*d/n: toast;
     toast = p/(n*np) < toast ? p/(n*np):toast;
-    
+
     cout << toast;
-    
+
     return 0;
 }
