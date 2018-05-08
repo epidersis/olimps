1952240d5bfa702a28d08c13191c05f9cc179e14
epidersis <epidersis@gmail.com@>
2018-05-08 20:06:40 +0900

    8.5.18

---
+++ 327B/327B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int n, i = 100000;
+    
+    cin >> n;
+    
+    while(n--)
+    {
+        cout << i++ << " ";
+    }
+
+    return 0;
+}
+
