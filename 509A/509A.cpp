339058ecff919ba9ab6eadcc83e12b17210ef887
epidersis <epidersis@gmail.com@>
2018-05-08 16:54:50 +0900

    may the force be with you

---
+++ 509A/509A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    short n;
+    cin >> n;
+    if ( n == 1 ) cout << 1;
+    else if ( n == 2 ) cout << 2;
+    else if ( n == 3 ) cout << 6;
+    else if ( n == 4 ) cout << 20;
+    else if ( n == 5 ) cout << 70;
+    else if ( n == 6 ) cout << 252;
+    else if ( n == 7 ) cout << 924;
+    else if ( n == 8 ) cout << 3432;
+    else if ( n == 9 ) cout << 12870;
+    else if ( n == 10 ) cout << 48620;    
+}
