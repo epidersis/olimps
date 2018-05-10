d6a1a0f8bc23cfd1a60d1e74d3d9d357bc6c4ba7
epidersis <epidersis@gmail.com@>
2018-05-10 17:40:10 +0900

    added 149A

--- 149A/149A.cpp
+++ 149A/149A.cpp
@@ -5,32 +5,34 @@ using namespace std;
 
 int main()
 {
-	short k;
-	cin >> k;
-	
-	if (k == 0)
-	{
-		cout << 0;
-		return 0;
-	}
-	
-	short arr[12];
-	
-	for (short i = 0; i < 12; i++) cin >> arr[i];
-	
-	sort(arr, arr+12);
-	
-	for (short i = i; i < 12; i++)
-	{
-		k -= arr[11 - i];
-		
-		if (k <= 0){
-			cout << i+1;
-			return 0;
-		}
-	}
-	
-	cout << -1;
-    return 0;
+    short k, count = 0, sum = 0;
+    
+    cin >> k;
+    
+    short a[12];
+    
+    for ( int i = 0; i < 12; i++ )
+    {
+        cin >> a[i];
+    }
+    
+    sort(a,a+12);
+    
+    for ( int i = 11; i >= 0; i-- )
+    {
+        if ( k == 0 )
+        {
+            break;
+        }
+        sum += a[i];
+        count++;
+        if ( sum >= k ) 
+        {
+            break;
+        }
+        
+    }
+    
+    if ( sum < k ) cout << -1;
+    else cout << count;
 }
-
