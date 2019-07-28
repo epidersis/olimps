ed958e217e2de63416bdb1ea14d8bc2d7a84084d
epidersis <32263760+epidersis@users.noreply.github.com>
2019-07-28 17:30:00 +0900

    Create 939A.js
    alive again

---
+++ 939A/939A.js
@@ -0,0 +1,23 @@
+var doesntExist = true;
+
+var n = parseInt(readline());
+var planes = readline().split(' ').map(function(x) {
+    return parseInt(x);
+});
+
+for (var i = 0; i < n; i++) {
+    firstPlanesLove = planes[i];
+    secondPlanesLove = planes[firstPlanesLove - 1];
+
+    thirdPlanesLove = planes[secondPlanesLove - 1];
+
+    if (thirdPlanesLove === i + 1) {
+        print('YES');
+        doesntExist = false;
+        break;
+    }
+}
+
+if (doesntExist) {
+    print('NO');
+}
